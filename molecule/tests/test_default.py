8a77bd191cb8b5415c04b21dd3669c50e74ed1cd
Karol Kurpanik <kkurpanik@gmail.com>
2021-10-02 17:33:22 +0200

    add more tests

---
+++ molecule/tests/test_default.py
@@ -0,0 +1,11 @@
+def test_is_nginx_installed(host):
+    assert host.package("nginx").is_installed
+
+def test_service_nginx_running(host):
+    assert host.service("nginx").is_running is True
+
+def test_is_mongodb_installed(host):
+    assert host.package("mongodb-org-server").is_installed
+
+def test_service_mongodb_running(host):
+    assert host.service("mongod").is_running is True
